62bb27b78fafae30e8c76f803c6e805a4f688a58
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2024-07-03 07:41:14 +0530

    some changes

--- main.py
+++ main.py
@@ -3,9 +3,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        "id": 1,
+        "title": "Data Scientist",
+        "location": "Bengaluru",
+        "salary": "Rs 80,000",
+    },
+    {
+        "id": 2,
+        "title": "Data Analyst",
+        "location": "Delhi",
+        "salary": "Rs 100,000",
+    },
+    {
+        "id": 3,
+        "title": "Backend Developer",
+        "location": "New York",
+    },
+    {
+        "id": 4,
+        "title": "Frontend",
+        "location": "Australia",
+        "salary": "Rs 20,000",
+    },
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
